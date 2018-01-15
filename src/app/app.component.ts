e740f436aba584837a4eece92d409fc828a4ce34
peixinchen <peixinchen@foxmail.com>
2018-01-15 22:10:01 +0800

    6 charts with bootstrap grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  currencies: string[] = [
+    "BTC",
+    "BCH",
+    "ETH",
+    "XRP",
+    "LTC",
+    "DSH",
+  ];
 }
--- src/app/candle/candle.component.ts
+++ src/app/candle/candle.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, Input } from '@angular/core';
 import * as Highcharts from 'highcharts/js/highcharts';
 
 @Component({
   selector: 'app-candle',
   templateUrl: './candle.component.html',
-  styleUrls: ['./candle.component.css']
+  styleUrls: ['./candle.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CandleComponent implements OnInit {
 
+  @Input() currency: string;
+
+  @ViewChild('chartTarget') chartTarget: ElementRef;
+  chart: Highcharts.ChartObject;
+
   constructor() { }
 
-  ngOnInit() {
-    Highcharts.chart('container', {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.chart = Highcharts.chart(this.chartTarget.nativeElement, {
       chart: {
         type: 'bar',
+      },
+
+      title: {
+        text: this.currency,
       },
 
       xAxis: {
@@ -34,6 +46,10 @@ export class CandleComponent implements OnInit {
         data: [5, 7, 3],
       }],
     });
+  }
+
+  ngOnDestroy() {
+    this.chart = null;
   }
 
 }
